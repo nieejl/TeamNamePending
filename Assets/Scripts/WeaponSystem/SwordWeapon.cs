dea8180a69ea523d4877685a635ebea0ca24d05f
vldtz <vlad.vana97@gmail.com>
2021-05-08 13:00:57 +0300

    wip: begin implementing the weapon system

---
+++ Assets/Scripts/WeaponSystem/SwordWeapon.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class SwordWeapon : Weapon
+{
+    public new float Damage = 10f;
+    public new int AttacksToBreak = 5;
+    
+    private int attackCounter = 0;
+
+    private Transform wholeMesh;
+    private Transform brokenMesh;
+
+    private void Awake()
+    {
+        SetupMeshes();
+    }
+
+    private void SetupMeshes()
+    {
+        wholeMesh = transform.GetChild(0);
+        brokenMesh = transform.GetChild(1);
+        wholeMesh.gameObject.SetActive(true);
+        brokenMesh.gameObject.SetActive(false);
+    }
+
+    public override void TryDoHeavyAttack()
+    {
+        attackCounter += 2;
+        if (attackCounter >= AttacksToBreak)
+        {
+            BreakWeapon();
+            return;
+        }
+
+        //do heavy animation
+    }
+
+    public override void TryDoLightAttack()
+    {
+        attackCounter++;
+        if (attackCounter >= AttacksToBreak)
+        {
+            BreakWeapon();
+            return;
+        }
+
+        //do light animation
+    }
+
+    public override void BreakWeapon()
+    {
+        wholeMesh.gameObject.SetActive(false);
+        brokenMesh.gameObject.SetActive(true);
+    }
+
+    public override void RepairWeapon()
+    {
+        attackCounter = 0;
+        wholeMesh.gameObject.SetActive(true);
+        brokenMesh.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<IDamageable>(out var damageable))
+            damageable.TakeDamage(Damage);
+    }
+}
