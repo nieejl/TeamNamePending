0cace95d245d79ed77ea21021074b799460fd422
KristianHedeholm <kristian.hedeholm@gmail.com>
2021-05-08 13:41:28 +0200

    Added a health system for the player based on scriptable objects, so it's easy to get the data for other parts of the game as well.

---
+++ Assets/Scripts/ScritableObject/PlayerData.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    public int CurrentValue { get; private set; }
+
+    public event Action<int> ChangedToValue;
+    public event Action ValueReachedZero;
+
+    [SerializeField]
+    private int _startAmount;
+    [SerializeField]
+    private int _maximumAmount;
+
+    private int _currentValue;
+
+    private void OnEnable()
+    {
+        _currentValue = _startAmount;
+    }
+
+    public void IncreaseValue(int increaseValue)
+    {
+        _currentValue += increaseValue;
+        if (_currentValue >= _maximumAmount)
+        {
+            _currentValue = _maximumAmount;
+        }
+        ChangedToValue?.Invoke(_currentValue);
+    }
+
+    public void DecreaseValue(int decreaseValue)
+    {
+        _currentValue -= decreaseValue;
+        if (_currentValue <= 0)
+        {
+            _currentValue = 0;
+            ValueReachedZero?.Invoke();
+        }
+        else
+        {
+            ChangedToValue?.Invoke(_currentValue);
+        }
+    }
+}
