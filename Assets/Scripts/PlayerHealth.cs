365bf818e771cec57e2b7cbcf7af4290b2ddccb7
KristianHedeholm <kristian.hedeholm@gmail.com>
2021-05-08 16:47:33 +0200

    Fixed so the Playerhealth singelton works with the scriptable objects.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public static readonly PlayerHealth Instance = new PlayerHealth();
+    public static PlayerHealth Instance { get; private set; }
     [SerializeField]
     private PlayerData _playerHealthData;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
 
     public void IncreaseHealth(int increaseValue)
     {
